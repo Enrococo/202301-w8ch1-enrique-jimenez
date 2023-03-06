acbba70cb6771a1cc7d786c5ec878b50ab3ddfb1
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-06 19:37:03 +0100

    Add not found page and tests

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { NotFoundStyled } from './NotFoundStyled';
+
+export const NotFound = () => {
+  return (
+    <NotFoundStyled>
+      <h1 className="title">Error 404</h1>
+      <p className="text">Looks like you got lost on the dark side</p>
+      <img
+        className="image"
+        src="/assets/images/not-found.png"
+        alt="page error 404"
+      />
+      <Link className="text" data-testid="link" to={'/'}>
+        Go home
+      </Link>{' '}
+    </NotFoundStyled>
+  );
+};
